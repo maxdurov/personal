a792438972ba6c06fcd823e3659ee1c80e3b3711
Max <70074382+maxdurov@users.noreply.github.com>
2024-07-07 18:53:46 +0300

    fix

---
+++ _next/static/XLYw6ZzASRJWhhca13zqB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/94726e6d-c4cbb5dcf369a3dc.js","static/chunks/221-6e23035cdc3aff83.js","static/chunks/pages/index-ee322fc3eb0300b1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
